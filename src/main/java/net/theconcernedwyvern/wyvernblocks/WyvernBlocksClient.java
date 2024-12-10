09650bf1547012e4e48bb4c08ed38638b6a6bb2a
theConcernedWyvern <theconcernedwyvern@gmail.com>
2024-12-10 00:48:32 -0500

    Fixed leaded glass transparency issue

--- src/main/java/net/theconcernedwyvern/wyvernblocks/WyvernBlocksClient.java
+++ src/main/java/net/theconcernedwyvern/wyvernblocks/WyvernBlocksClient.java
@@ -1,10 +1,15 @@
 package net.theconcernedwyvern.wyvernblocks;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.theconcernedwyvern.wyvernblocks.block.ModBlocks;
 
 public class WyvernBlocksClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LEADED_GLASS, RenderLayer.getCutout());
 
     }
 }
