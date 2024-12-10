1969edce20e112c8f1256e681885ef9d8c915173
theConcernedWyvern <theconcernedwyvern@gmail.com>
2024-12-09 22:46:52 -0500

    Initial commit

---
+++ src/main/java/net/theconcernedwyvern/wyvernblocks/WyvernBlocksClient.java
@@ -0,0 +1,10 @@
+package net.theconcernedwyvern.wyvernblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WyvernBlocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
